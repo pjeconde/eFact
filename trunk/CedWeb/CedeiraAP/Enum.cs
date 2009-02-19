c0a532b77865a1e239b552b1a2fd898683c2a616
lucas.legaspi@gmail.com <lucas.legaspi@gmail.com@07d05888-fd1f-11dd-8c4e-4bbb75a408b7>
2009-02-19 18:02:41 +0000

    Removed file/folder

--- trunk/CedWeb/CedeiraAP/Enum.cs
+++
@@ -1,8 +0,0 @@
-using System;
-namespace Cedeira.SV
-{
-	public enum ModoEnum {Alta, Baja, AnulacionBaja, Modificacion, Consulta, Copia, Sincronizacion, Seleccionar, ModificacionId}
-	public enum EstadoEnum {Vigente=65, DeBaja=66}
-	public enum BrowserModoEnum {Ingreso, Autorizacion, Consulta, Exploracion}
-	public enum DepuracionModoEnum {Dejar, Borrar}
-}
