17de6060641a43087266f947abe383a48dd4c9a3
lucas.legaspi@gmail.com <lucas.legaspi@gmail.com@07d05888-fd1f-11dd-8c4e-4bbb75a408b7>
2009-03-19 19:03:45 +0000

    Comprobantes en dólares. Falta el upload.

---
+++ trunk/CedWeb/FeaEntidades/CodigosMoneda/DolarUSA.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeaEntidades.CodigosMoneda
+{
+	public class DolarUSA : CodigoMoneda
+	{
+		public DolarUSA()
+		{
+			Codigo = "DOL";
+			Descr = "Dólar ESTADOUNIDENSE";
+		}
+	}
+}
---
+++ trunk/CedWeb/FeaEntidades/CodigosMoneda/Pesos.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeaEntidades.CodigosMoneda
+{
+	public class Pesos : CodigoMoneda
+	{
+		public Pesos()
+		{
+			Codigo = "PES";
+			Descr = "PESOS";
+		}
+	}
+}
