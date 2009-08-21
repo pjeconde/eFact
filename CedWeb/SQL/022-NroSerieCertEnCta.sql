1ce15f8cef65b0cb7d904ca44c54571700236128
lucas.legaspi <lucas.legaspi@gmail.com>
2009-08-21 14:11:54 +0000

    me olvide de cuenta depurada en el script

--- CedWeb/SQL/022-NroSerieCertEnCta.sql
+++ CedWeb/SQL/022-NroSerieCertEnCta.sql
@@ -8,3 +8,14 @@ UPDATE dbo.Cuenta SET NroSerieCertificado=''
 ALTER TABLE dbo.Cuenta ALTER COLUMN
 	NroSerieCertificado varchar(50) NOT NULL
 GO
+
+ALTER TABLE dbo.CuentaDepurada ADD
+	NroSerieCertificado varchar(50) NULL
+GO
+
+UPDATE dbo.CuentaDepurada SET NroSerieCertificado=''
+ GO
+
+ALTER TABLE dbo.CuentaDepurada ALTER COLUMN
+	NroSerieCertificado varchar(50) NOT NULL
+GO
