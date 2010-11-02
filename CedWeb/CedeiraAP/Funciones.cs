6acb9559f0bacc565d2e7a858cefb443dbb46d38
lucas.legaspi <lucas.legaspi@gmail.com>
2010-11-02 20:34:54 +0000

    MD5 para contraseña de aviso

--- CedWeb/CedeiraAP/Funciones.cs
+++ CedWeb/CedeiraAP/Funciones.cs
@@ -5,6 +5,7 @@ using System.Web.Security;
 using System.Security.Principal;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
+using System.Text;
 namespace Cedeira.SV
 {
 	/// <summary>
@@ -456,5 +457,23 @@ namespace Cedeira.SV
 			impersonationContext.Undo();
 		}
 		#endregion
+		public static string CreateMD5Hash(string input)
+		{
+			// Use input string to calculate MD5 hash
+			System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+			byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+			byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+			// Convert the byte array to hexadecimal string
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < hashBytes.Length; i++)
+			{
+				sb.Append(hashBytes[i].ToString("X2"));
+				// To force the hex string to lower-case letters instead of
+				// upper-case, use he following line instead:
+				// sb.Append(hashBytes[i].ToString("x2")); 
+			}
+			return sb.ToString();
+		}
 	}
 }
