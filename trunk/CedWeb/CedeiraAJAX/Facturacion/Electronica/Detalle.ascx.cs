8067d2f02e569cacfd6ba1c9d59f72d152ff2a09
lucas.legaspi@gmail.com <lucas.legaspi@gmail.com@07d05888-fd1f-11dd-8c4e-4bbb75a408b7>
2013-03-01 19:38:42 +0000

    Bug alícuota iva e importe total RG2904

--- trunk/CedWeb/CedeiraAJAX/Facturacion/Electronica/Detalle.ascx.cs
+++ trunk/CedWeb/CedeiraAJAX/Facturacion/Electronica/Detalle.ascx.cs
@@ -963,17 +963,17 @@ namespace CedeiraAJAX.Facturacion.Electronica
 				if (MonedaComprobante.Equals(FeaEntidades.CodigosMoneda.CodigoMoneda.Local))
 				{
 
-					GenerarDetalleMonedaLocal(TipoCbte, det, listadelineas, i);
+                    GenerarDetalleMonedaLocal(TipoCbte, det, listadelineas, i, TipoPtoVta);
 				}
 				else
 				{
-					GenerarDetalleMonedaExtranjera(TipoDeCambio, TipoCbte, det, listadelineas, i);
+                    GenerarDetalleMonedaExtranjera(TipoDeCambio, TipoCbte, det, listadelineas, i, TipoPtoVta);
 				}
 			}
 			return det;
 		}
 
-		private static void GenerarDetalleMonedaExtranjera(string TipoDeCambio, string TipoCbte, FeaEntidades.InterFacturas.detalle det, System.Collections.Generic.List<FeaEntidades.InterFacturas.linea> listadelineas, int i)
+        private static void GenerarDetalleMonedaExtranjera(string TipoDeCambio, string TipoCbte, FeaEntidades.InterFacturas.detalle det, System.Collections.Generic.List<FeaEntidades.InterFacturas.linea> listadelineas, int i, string TipoPtoVta)
 		{
 			det.linea[i].precio_unitarioSpecified = listadelineas[i].precio_unitarioSpecified;
 			switch (TipoCbte)
@@ -1031,7 +1031,14 @@ namespace CedeiraAJAX.Facturacion.Electronica
 					{
 						limo.precio_unitario = Math.Round(listadelineas[i].precio_unitario, 3);
 					}
-					limo.importe_total_articulo = Math.Round(listadelineas[i].importe_total_articulo + listadelineas[i].importe_iva, 2);
+                    if (TipoPtoVta.Equals("RG2904"))
+                    {
+                        limo.importe_total_articulo = Math.Round(listadelineas[i].importe_total_articulo, 2);
+                    }
+                    else
+                    {
+                        limo.importe_total_articulo = Math.Round(listadelineas[i].importe_total_articulo + listadelineas[i].importe_iva, 2);
+                    }
 					break;
 				default:
 					limo.importe_ivaSpecified = listadelineas[i].importe_ivaSpecified;
@@ -1043,29 +1050,36 @@ namespace CedeiraAJAX.Facturacion.Electronica
 			det.linea[i].importes_moneda_origen = limo;
 		}
 
-		private static void GenerarDetalleMonedaLocal(string TipoCbte, FeaEntidades.InterFacturas.detalle det, System.Collections.Generic.List<FeaEntidades.InterFacturas.linea> listadelineas, int i)
+        private static void GenerarDetalleMonedaLocal(string TipoCbte, FeaEntidades.InterFacturas.detalle det, System.Collections.Generic.List<FeaEntidades.InterFacturas.linea> listadelineas, int i, string TipoPtoVta)
 		{
-			switch (TipoCbte)
-			{
-				case "6":
-				case "7":
-				case "8":
-				case "9":
-				case "10":
-				case "40":
-				case "61":
-				case "64":
-					if (!listadelineas[i].alicuota_iva.Equals(new FeaEntidades.IVA.SinInformar().Codigo))
-					{
-						det.linea[i].precio_unitario = Math.Round(listadelineas[i].precio_unitario * (1 + listadelineas[i].alicuota_iva / 100), 3);
-					}
-					else
-					{
-						det.linea[i].precio_unitario = Math.Round(listadelineas[i].precio_unitario, 3);
-					}
-					det.linea[i].importe_total_articulo = Math.Round(listadelineas[i].importe_total_articulo + listadelineas[i].importe_iva, 2);
-					det.linea[i].importe_ivaSpecified = false;
-					det.linea[i].importe_iva = 0;
+            switch (TipoCbte)
+            {
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "10":
+                case "40":
+                case "61":
+                case "64":
+                    if (!listadelineas[i].alicuota_iva.Equals(new FeaEntidades.IVA.SinInformar().Codigo))
+                    {
+                        det.linea[i].precio_unitario = Math.Round(listadelineas[i].precio_unitario * (1 + listadelineas[i].alicuota_iva / 100), 3);
+                    }
+                    else
+                    {
+                        det.linea[i].precio_unitario = Math.Round(listadelineas[i].precio_unitario, 3);
+                    }
+                    if (TipoPtoVta.Equals("RG2904"))
+                    {
+                        det.linea[i].importe_total_articulo = Math.Round(listadelineas[i].importe_total_articulo, 2);
+                    }
+                    else
+                    {
+                        det.linea[i].importe_total_articulo = Math.Round(listadelineas[i].importe_total_articulo + listadelineas[i].importe_iva, 2);
+                    }
+                    det.linea[i].importe_ivaSpecified = false;
+                    det.linea[i].importe_iva = 0;
 					break;
 				default:
 					det.linea[i].precio_unitario = listadelineas[i].precio_unitario;
@@ -1131,10 +1145,21 @@ namespace CedeiraAJAX.Facturacion.Electronica
 					case "10":
 					case "40":
 					case "61":
-					case "64":
-						det.linea[i].alicuota_ivaSpecified = false;
-						det.linea[i].alicuota_iva = 0;
-						break;
+                    case "64":
+                        if (TipoPtoVta.Equals("RG2904"))
+                        {
+                            det.linea[i].alicuota_ivaSpecified = listadelineas[i].alicuota_ivaSpecified;
+                            if (!listadelineas[i].alicuota_iva.Equals(new FeaEntidades.IVA.SinInformar().Codigo))
+                            {
+                                det.linea[i].alicuota_iva = listadelineas[i].alicuota_iva;
+                            }
+                        }
+                        else
+                        {
+                            det.linea[i].alicuota_ivaSpecified = false;
+                            det.linea[i].alicuota_iva = 0;
+                        }
+                        break;
 					default:
 						det.linea[i].alicuota_ivaSpecified = listadelineas[i].alicuota_ivaSpecified;
 						if (!listadelineas[i].alicuota_iva.Equals(new FeaEntidades.IVA.SinInformar().Codigo))
@@ -1143,7 +1168,6 @@ namespace CedeiraAJAX.Facturacion.Electronica
 						}
 						break;
 				}
-
 			}
 		}
 
